107ef73f234ce7fdbf262e60d51c5f3d9e2bbae3
osdever <krasota156@gmail.com>
2023-01-10 23:52:49 +0300

    [all] Modify the project to download Re

--- src/setup-re.ts
+++ src/setup-re.ts
@@ -4,7 +4,7 @@ import { promises as fsPromises } from 'fs';
 import * as path from 'path';
 import * as vi from './version-info';
 
-const PACKAGE_NAME: string = 'cmake';
+const PACKAGE_NAME: string = 're-build-system';
 
 function getURL(
   version: vi.VersionInfo,
@@ -13,6 +13,7 @@ function getURL(
   const assets_for_platform: vi.AssetInfo[] = version.assets
     .filter((a) => a.platform === process.platform && a.filetype === 'archive')
     .sort();
+
   // The arch_candidates provides an ordered set of architectures to try, and
   // the first matching asset is used. This will typically be 'x86_64' first,
   // with 'x86' checked if nothing was found.
@@ -27,16 +28,12 @@ function getURL(
   if (matching_assets == undefined) {
     // If there are no x86_64 or x86 packages then give up.
     throw new Error(
-      `Could not find ${process.platform} asset for cmake version ${version.name}`
+      `Could not find ${process.platform} asset for Re version ${version.name}`
     );
   }
   if (matching_assets.length > 1) {
     core.warning(`Found ${matching_assets.length} matching packages.`);
-    // If there are multiple assets it is likely to be because there are MacOS
-    // builds for PPC, x86 and x86_64. Universal packages prevent parsing the
-    // architecture completely, so we need to match against the full url to
-    // differentiate between e.g. cmake-2.8.10.2-Darwin-universal.tar.gz and
-    // cmake-2.8.10.2-Darwin64-universal.tar.gz.
+    
     // Check to see if this narrows down the options or just removes all options.
     // Prefer to use all previous matches when none of them include '64'.
     const possible_assets = matching_assets.filter((a) => a.url.match('64'));
@@ -64,7 +61,7 @@ async function getArchive(url: string): Promise<string> {
   }
 }
 
-export async function addCMakeToToolCache(
+export async function addReToToolCache(
   version: vi.VersionInfo,
   arch_candidates: Array<string>
 ): Promise<string> {
@@ -73,6 +70,10 @@ export async function addCMakeToToolCache(
 }
 
 async function getBinDirectoryFrom(tool_path: string): Promise<string> {
+  // TODO: this will likely not work on macOS!
+  return tool_path;
+
+  /*
   // The cmake archive should have a single top level directory with a name
   // similar to 'cmake-3.16.2-win64-x64'. This then has subdirectories 'bin',
   // 'doc', 'share'.
@@ -90,15 +91,16 @@ async function getBinDirectoryFrom(tool_path: string): Promise<string> {
   } else {
     return path.join(tool_path, root_dir_path[0], 'bin');
   }
+  */
 }
 
-export async function addCMakeToPath(
+export async function addReToPath(
   version: vi.VersionInfo,
   arch_candidates: Array<string>
 ): Promise<void> {
   let tool_path: string = tc.find(PACKAGE_NAME, version.name);
   if (!tool_path) {
-    tool_path = await addCMakeToToolCache(version, arch_candidates);
+    tool_path = await addReToToolCache(version, arch_candidates);
   }
   await core.addPath(await getBinDirectoryFrom(tool_path));
 }
